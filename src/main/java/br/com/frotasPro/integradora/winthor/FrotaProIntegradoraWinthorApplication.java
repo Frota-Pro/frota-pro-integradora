393bba8f7b8eeea7e0f8cb7aed59e4cc9949c07b
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2025-11-18 13:47:49 -0300

    Initial commit - Projeto criado com Spring Initializr

---
+++ src/main/java/br/com/frotasPro/integradora/winthor/FrotaProIntegradoraWinthorApplication.java
@@ -0,0 +1,13 @@
+package br.com.frotasPro.integradora.winthor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrotaProIntegradoraWinthorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrotaProIntegradoraWinthorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/frotasPro/integradora/winthor/FrotaProIntegradoraWinthorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.frotasPro.integradora.winthor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrotaProIntegradoraWinthorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
